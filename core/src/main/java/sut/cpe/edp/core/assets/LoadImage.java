2ad696b36aa72b16134d5cddeb090c6924047477
ronsc <fdroncpe17@gmail.com>
2015-05-30 18:16:29 +0700

    click to start

---
+++ core/src/main/java/sut/cpe/edp/core/assets/LoadImage.java
@@ -0,0 +1,10 @@
+package sut.cpe.edp.core.assets;
+
+import static playn.core.PlayN.*;
+
+import playn.core.Image;
+
+public class LoadImage {
+    public Image bgStartScreen = assets().getImage("images/bg_startgame.png");
+    public Image bgStripe = assets().getImage("images/bg_stripe.png");
+}
--- core/src/main/java/sut/cpe/edp/core/characters/Golang.java
+++ core/src/main/java/sut/cpe/edp/core/characters/Golang.java
@@ -1,7 +1,15 @@
 package sut.cpe.edp.core.characters;
 
+import org.jbox2d.collision.shapes.PolygonShape;
+import org.jbox2d.common.Vec2;
+import org.jbox2d.dynamics.*;
+import playn.core.Key;
+import playn.core.Keyboard;
 import playn.core.Layer;
+import playn.core.PlayN;
 import playn.core.util.Callback;
+import playn.core.util.Clock;
+import sut.cpe.edp.core.assets.LoadWorld;
 import sut.cpe.edp.core.sprites.Sprite;
 import sut.cpe.edp.core.sprites.SpriteLoader;
 
@@ -9,21 +17,36 @@ public class Golang {
 
     private Sprite sprite;
     private int spriteIndex = 0;
-    private boolean hasLoaded = false;
+    private boolean hasLoaded = false, hasStart = false;
 
     public enum State { IDLE };
 
     private int e = 0, offset = 0;
 
-    public Golang(final float x, final float y) {
+    private Body golangBody;
+
+    public Golang(final World world, final float x, final float y) {
         sprite = SpriteLoader.getSprite("json/golang.json");
         sprite.addCallback(new Callback<Sprite>() {
             @Override
             public void onSuccess(Sprite result) {
                 sprite.setSprite(spriteIndex);
-                sprite.layer().setOrigin(sprite.width(), sprite.height());
+                sprite.layer().setOrigin(sprite.width() / 2f, sprite.height() / 2f);
                 sprite.layer().setTranslation(x, y);
                 hasLoaded = true;
+
+                golangBody = initPhysicsBox(world, x, y, sprite.width()/2f, sprite.height()/2f);
+
+                PlayN.keyboard().setListener(new Keyboard.Adapter() {
+                    @Override
+                    public void onKeyUp(Keyboard.Event event) {
+                        if (event.key().equals(Key.SPACE)) {
+                            if(hasStart) {
+                                golangBody.applyForce(new Vec2(0, -150), golangBody.getPosition());
+                            }
+                        }
+                    }
+                });
             }
 
             @Override
@@ -42,9 +65,48 @@ public class Golang {
             sprite.setSprite(spriteIndex);
             e = 0;
         }
+
+        if(golangBody.getPosition().y > 9 && !hasStart) {
+            golangBody.applyForce(new Vec2(0, -40), golangBody.getPosition());
+        }
+    }
+
+    public void paint(Clock clock){
+        if(!hasLoaded) return;
+//        sprite.layer().setTranslation(
+//                golangBody.getPosition().x / LoadWorld.M_PER_PIXEL,
+//                golangBody.getPosition().y / LoadWorld.M_PER_PIXEL
+//        );
+    }
+
+    public void setHasStart(boolean hasStart) {
+        this.hasStart = hasStart;
     }
 
     public Layer layer() {
         return sprite.layer();
+    }
+
+    public Body initPhysicsBox(World world, float x, float y, float w, float h) {
+        BodyDef bodyDef = new BodyDef();
+        bodyDef.type = BodyType.DYNAMIC;
+
+        Body box = world.createBody(bodyDef);
+        PolygonShape boxShape = new PolygonShape();
+        boxShape.setAsBox(w * LoadWorld.M_PER_PIXEL, h * LoadWorld.M_PER_PIXEL);
+
+        FixtureDef fixtureDef = new FixtureDef();
+        fixtureDef.shape = boxShape;
+        fixtureDef.density = 0.2f;
+        fixtureDef.friction = 0.2f;
+        fixtureDef.restitution = 0.5f;
+        box.createFixture(fixtureDef);
+
+        box.setTransform(new Vec2(
+                x * LoadWorld.M_PER_PIXEL,
+                y * LoadWorld.M_PER_PIXEL
+        ), 0f);
+
+        return box;
     }
 }
